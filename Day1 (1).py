3c9aa7802013c92d1989dde25111d5452f141ecd
MARUTHIPRASADREDDY <164727823+MARUTHIPRASADREDDY@users.noreply.github.com>
2024-04-02 23:39:48 +0530

    Add files via upload

---
+++ Day1 (1).py
@@ -0,0 +1,51 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 8,
+   "id": "0f2473d3-0123-4248-9f06-dcd3ea31c65c",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Hello all welocome to the python programming world\n"
+     ]
+    }
+   ],
+   "source": [
+    "print(\"Hello all welocome to the python programming world\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "7d31265d-6df4-42be-9c3c-88ef8519a404",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "anaconda-panel-2023.05-py310",
+   "language": "python",
+   "name": "conda-env-anaconda-panel-2023.05-py310-py"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.11.5"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
